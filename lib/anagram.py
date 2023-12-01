6089a50d84c41c8ab6323185028c607092dbdca6
joshua chivile <joshuachivile@gmail.com>
2023-12-01 17:10:29 +0300

    Anagram code challenge

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+    
+    def match(self,match_list):
+       return [item for item in match_list if sorted(item) == sorted(self.word)]
+
+word = Anagram("enlist")
+print(word.match(["listen", "silent", "hippopotamus"]))
+
